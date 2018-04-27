39a98c4748ca6c263c193d1fe24c82bc202e3bb8
Geoffrey Cheah <geoffcheah@gmail.com>
2018-04-27 15:44:33 +0100

    fixed seed and a runner validation

--- app/models/runner.rb
+++ app/models/runner.rb
@@ -1,11 +1,11 @@
 class Runner < ApplicationRecord
   belongs_to :area
   has_many :memberships, dependent: :destroy
-  belongs_to :user, through: :areas
+  # belongs_to :user, through: :areas
 
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :status, presence: true, inclusion: { in: ["never_run", "regular", "lapsed"] }
   validates :email, presence: true
-  validates :area, presence: true, inclusion: { in: ["Southwark", "Westminster", "Islington", "Camden"] }
+  validates :area, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,40 +1,39 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 puts "starting seed"
 
+puts "destroying previous seeds"
+Runner.destroy_all
+Preference.destroy_all
+User.destroy_all
+Area.destroy_all
+
 puts "creating area"
-westminster = Area.new(name: "Westminster")
+westminster = Area.create!(name: "Westminster")
 
 puts "creating user (trainer)"
-user = User.new(first_name: "Axel", last_name: "Manzano", email: "am@gmail.com", password: "123456" area: westminster)
+user = User.create!(first_name: "Axel", last_name: "Manzano", email: "am@gmail.com", password: "123456", area: westminster)
 
 puts "creating runners"
-john = Runner.new(email: "jd@gmail.com", first_name: "John", last_name: "Doe", status: "never_run", area: westminster)
-henry = Runner.new(email: "hc@gmail.com", first_name: "Henry", last_name: "Cavill", status: "lapsed", area: westminster)
-arnold = Runner.new(email: "as@gmail.com", first_name: "Arnold", last_name: "Schwarzenegger", status: "regular", area: westminster)
-ellen = Runner.new(email: "ed@gmail.com", first_name: "Ellen", last_name: "Degenneres", status: "never_run", area: westminster)
-theresa = Runner.new(email: "tm@gmail.com", first_name: "Theresa", last_name: "May", status: "lapsed", area: westminster)
+john = Runner.create!(email: "jd@gmail.com", first_name: "John", last_name: "Doe", status: "never_run", area: westminster)
+henry = Runner.create!(email: "hc@gmail.com", first_name: "Henry", last_name: "Cavill", status: "lapsed", area: westminster)
+arnold = Runner.create!(email: "as@gmail.com", first_name: "Arnold", last_name: "Schwarzenegger", status: "regular", area: westminster)
+ellen = Runner.create!(email: "ed@gmail.com", first_name: "Ellen", last_name: "Degenneres", status: "never_run", area: westminster)
+theresa = Runner.create!(email: "tm@gmail.com", first_name: "Theresa", last_name: "May", status: "lapsed", area: westminster)
 
 puts "creating preferences"
 
-mission = Preference.new(name: "Mission")
-group_run = Preference.new(name: "GroupRun")
-coach_run = Preference.new(name: "CoachRun")
+mission = Preference.create!(name: "Mission")
+group_run = Preference.create!(name: "GroupRun")
+coach_run = Preference.create!(name: "CoachRun")
 
 puts "creating memberships"
 
-john_membership1 = Membership.new(runner: john, preference: mission)
-john_membership2 = Membership.new(runner: john, preference: group_run)
-henry_membership = Membership.new(runner: henry, preference: coach_run)
-arnold_membership1 = Membership.new(runner: arnold, preference: mission)
-arnold_membership2 = Membership.new(runner: arnold, preference: coach_run)
-ellen_membership = Membership.new(runner: ellen, preference: mission)
-theresa_membership = Membership.new(runner: theresa, preference: mission)
+john_membership1 = Membership.create!(runner: john, preference: mission)
+john_membership2 = Membership.create!(runner: john, preference: group_run)
+henry_membership = Membership.create!(runner: henry, preference: coach_run)
+arnold_membership1 = Membership.create!(runner: arnold, preference: mission)
+arnold_membership2 = Membership.create!(runner: arnold, preference: coach_run)
+ellen_membership = Membership.create!(runner: ellen, preference: mission)
+theresa_membership = Membership.create!(runner: theresa, preference: mission)
 
 puts "seed finished!"
 
