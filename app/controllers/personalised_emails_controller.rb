4545517fc25749d6f132bf30b4829cc3290fe30f
Geoffrey Cheah <geoffcheah@gmail.com>
2018-04-30 17:06:48 +0100

    moved filtering logic to personalised emails controller in order to send multiple emails to the different email addresses. Previous logic in the UserMailer rb file was sending multiple emails only to one email address.

--- app/controllers/personalised_emails_controller.rb
+++ app/controllers/personalised_emails_controller.rb
@@ -9,11 +9,16 @@ class PersonalisedEmailsController < ApplicationController
     @personalised_email = PersonalisedEmail.new(email_params)
     @personalised_email.user = current_user
     if @personalised_email.save!
-      UserMailer.contact(@personalised_email, @runners).deliver_now
-      flash[:notice] = "Email sent!"
-      redirect_to root_path
+      target_status = @personalised_email.status
+      @runners = filter_runners_for_status(@runners, target_status)
+      @runners = filter_runners_for_preference(@runners, @personalised_email)
+      @runners.each do |runner|
+        UserMailer.contact(@personalised_email, runner).deliver_now
+      end
+      flash[:alert] = "Email sent!"
+      redirect_to(root_path)
     else
-      flash[:errors] = @personalised_email.errors.full_messages
+      flash[:alert] = @personalised_email.errors.full_messages
       render 'new'
     end
   end
@@ -23,4 +28,32 @@ class PersonalisedEmailsController < ApplicationController
   def email_params
     params.require(:personalised_email).permit(:subject, :content, :status, :group_run, :mission, :coach_run, :user_id)
   end
+
+   def contact_group_runners?(personalised_email)
+    personalised_email.group_run
+  end
+
+  def contact_mission_runners?(personalised_email)
+    personalised_email.mission
+  end
+
+  def contact_coach_runners?(personalised_email)
+    personalised_email.coach_run
+  end
+
+  def filter_runners_for_status(runners, target_status)
+    runners.select { |runner| runner.status == target_status}
+  end
+
+  def filter_runners_for_preference(runners, personalised_email)
+    runners.delete_if { |runner| !runner.group_run } if contact_group_runners?(personalised_email)
+    runners.delete_if { |runner| !runner.mission } if contact_mission_runners?(personalised_email)
+    runners.delete_if { |runner| !runner.coach_run } if contact_coach_runners?(personalised_email)
+  end
 end
+
+# go through each runner
+# find runners where status matches
+# put runners in separate array to iterate over
+# find each runner who has given preference through looking at their boolean attributes,
+# use completely filtered runners array to send bulk emails
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,46 +5,9 @@ class UserMailer < ApplicationMailer
   #
   #   en.user_mailer.contact.subject
   #
-  def contact(personalised_email, runners)
+  def contact(personalised_email, runner)
     @personalised_email = personalised_email
-    @runners = runners
-    target_status = @personalised_email.status
-    @runners = filter_runners_for_status(@runners, target_status)
-    @runners = filter_runners_for_preference(@runners, @personalised_email)
-
-    @runners.each do |runner|
-      @runner = runner
-      mail(to: @runner.email , subject: "#{@personalised_email.subject}")
-    end
-  end
-
-  private
-
-  def contact_group_runners?(personalised_email)
-    personalised_email.group_run
-  end
-
-  def contact_mission_runners?(personalised_email)
-    personalised_email.mission
-  end
-
-  def contact_coach_runners?(personalised_email)
-    personalised_email.coach_run
-  end
-
-  def filter_runners_for_status(runners, target_status)
-    runners.select { |runner| runner.status == target_status}
-  end
-
-  def filter_runners_for_preference(runners, personalised_email)
-    runners.delete_if { |runner| !runner.group_run } if contact_group_runners?(personalised_email)
-    runners.delete_if { |runner| !runner.mission } if contact_mission_runners?(personalised_email)
-    runners.delete_if { |runner| !runner.coach_run } if contact_coach_runners?(personalised_email)
+    @runner = runner
+    mail(to: @runner.email , subject: "#{@personalised_email.subject}")
   end
 end
-
-# go through each runner
-# find runners where status matches
-# put runners in separate array to iterate over
-# find each runner who has given preference through looking at their boolean attributes,
-# altering runners array which is returned to runners_to_send_to
